34d26dac38c50222424b09305f8b9c5149a34bb2
Sangeevan <sangeevansiventhirarajah@gmail.com>
2020-08-19 17:19:02 +0530

    images

--- src/components/LoginComponent/LoginContainer.tsx
+++ src/components/LoginComponent/LoginContainer.tsx
@@ -1,21 +1,55 @@
-import { IonButton} from '@ionic/react';
-import React from 'react';
+import { IonButton, IonCol, IonGrid, IonImg, IonInput, IonItem, IonItemDivider, IonList, IonRow, IonThumbnail} from '@ionic/react';
+import React, { useState } from 'react';
 import './LoginContainer.css';
 
 interface ContainerProps { 
 }
 
+type Item = {
+  src: string;
+  text: string;
+};
+const items: Item[] = [{ src: 'http://placekitten.com/g/200/300', text: 'a picture of a cat' }];
+
+
+
 const LoginContainer: React.FC<ContainerProps> = () => {
+
+  const [userName, setUserName] = useState<string>();
+  const [password, setPassword] = useState<string>();
   
   return (
-    <div className="container">
-      <IonButton id="loginbtn" className="login-button" expand="block" onClick={()=>login()} >Sign in</IonButton>
+
+    <div className="containerLogin">   
+      <IonImg  className="img" src= ""/>
+         <br/>
+      <IonList> 
+          <IonItemDivider>Sign in to TransTick</IonItemDivider>
+          <IonItem>
+            <IonInput type="email" value={userName} placeholder="Email" onIonChange={e => setUserName(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput type="password" value={password} placeholder="Password" onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+          </IonItem>
+      </IonList>
+      <IonButton id="loginbtn" className="login-button" expand="block" onClick={()=>login(userName, password)} >Sign in</IonButton>
+      <br/>
+      <div className="center">
+      <p>Don't have an account?</p> 
+      <strong><a href="/signup">Sign up</a></strong>
+      </div>
     </div>
   );
 };
 
-function login(){
-  window.location.href = "/home";
+function login( userName: string | undefined, password:string | undefined){
+  let loginUser = false;
+  if(userName=="sangeevan" && password=="1995"){
+    loginUser = true;
+  }
+  if(loginUser){
+    window.location.href = "/home";
+  }
 }
 
 export default LoginContainer;
--- src/components/SignupComponent/SignupContainer.tsx
+++ src/components/SignupComponent/SignupContainer.tsx
@@ -1,15 +1,63 @@
-import React from 'react';
+import { IonButton, IonInput, IonItem, IonItemDivider, IonList } from '@ionic/react';
+import React, { useState } from 'react';
 import './SignupContainer.css';
 
 interface ContainerProps { }
 
 const SignupContainer: React.FC<ContainerProps> = () => {
+
+  const [userName, setUserName] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [confirmPassword, setConfirmPassword] = useState<string>();
+  const [phoneNumber, setPhoneNumber] = useState<string>();
+  const [address, setAddress] = useState<string>();
+  const [district, setDistrict] = useState<string>();
+  const [DOB, setDOB] = useState<string>();
+
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div className="containerSignup">
+      <IonList>
+          <IonItem>
+            <IonInput type="email" value={userName} placeholder="Email" onIonChange={e => setUserName(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput type="password" value={password} placeholder="Password" onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput type="password" value={confirmPassword} placeholder="Confirm Password" onIonChange={e => setConfirmPassword(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput type="number" value={phoneNumber} placeholder="Phone number" onIonChange={e => setPhoneNumber(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput type="text" value={address} placeholder="Address" onIonChange={e => setAddress(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput type="text" value={district} placeholder="District" onIonChange={e => setDistrict(e.detail.value!)}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput type="text" value={DOB} placeholder="Date of birth" onIonChange={e => setDOB(e.detail.value!)}></IonInput>
+          </IonItem>
+      </IonList>
+      <br/>
+      <IonButton id="loginbtn" className="login-button" expand="block" onClick={()=>signup(userName, password, confirmPassword)} >Create account</IonButton>
+      <br/>
+      <div className="center">
+      <p>Already have an account?</p> 
+      <strong><a href="/login">Log in</a></strong>
+      </div>    
     </div>
   );
 };
+
+function signup( userName: string | undefined, password:string | undefined,  confirmPassword:string | undefined){
+  let signupUser = false;
+  if(password==confirmPassword){
+    signupUser = true;
+  }
+  if(signupUser){
+    window.location.href = "/login";
+  }
+}
 
 export default SignupContainer;
