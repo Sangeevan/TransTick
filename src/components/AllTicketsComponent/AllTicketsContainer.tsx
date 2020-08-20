7e8c334a50dce95df6aeabcbd72fcc1d3243b837
Sangeevan <sangeevansiventhirarajah@gmail.com>
2020-08-20 23:47:56 +0530

    List

--- src/components/AllTicketsComponent/AllTicketsContainer.tsx
+++ src/components/AllTicketsComponent/AllTicketsContainer.tsx
@@ -1,13 +1,27 @@
+import { IonButton, IonCol, IonGrid, IonImg, IonInput, IonItem, IonItemDivider, IonLabel, IonList, IonRow, IonThumbnail} from '@ionic/react';
 import React from 'react';
 import './AllTicketsContainer.css';
 
 interface ContainerProps { }
 
 const AllTicketsContainer: React.FC<ContainerProps> = () => {
+
+const names = [{"key":"value1","key2":"value2","key3":"value3"},
+                {"key":"valuea","key2":"valueb","key3":"valuec"},
+                {"key":"valuea","key2":"valueb","key3":"valuec"},
+                {"key":"valuea","key2":"valueb","key3":"valuec"}];
+
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div>
+      <IonList>
+        {names.map(name => (
+          <IonItem>
+              <IonLabel>{name.key}</IonLabel>
+              <IonLabel>{name.key2}</IonLabel>
+              <IonLabel>{name.key3}</IonLabel>
+          </IonItem>
+        ))}
+      </IonList>
     </div>
   );
 };
